0fb400447ec5901b4cd8299333e7979e05773bd6
Alex Alexeyev <aasmc@yandex.ru>
2022-01-14 22:55:43 +0300

    Update gradle files and dependencies

--- buildSrc/src/main/java/Kotlinx.kt
+++ buildSrc/src/main/java/Kotlinx.kt
@@ -2,7 +2,7 @@ object Kotlinx {
     private const val kotlinxDatetimeVersion = "0.1.1"
     const val datetime = "org.jetbrains.kotlinx:kotlinx-datetime:$kotlinxDatetimeVersion"
 
-    private const val coroutinesCoreVersion = "1.5.21"
+    private const val coroutinesCoreVersion = "1.5.31"
     const val coroutinesCore = "org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesCoreVersion"
 
     // Need for tests. Plugin doesn't work.
